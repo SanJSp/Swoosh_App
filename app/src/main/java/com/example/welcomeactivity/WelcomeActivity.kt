2dff995019f5528c8f259f14df1ea0d4997bccf1
Sandro <sandro.speh@student.hpi.de>
2019-03-29 17:25:59 +0100

    added Button functionality

--- app/src/main/java/com/example/welcomeactivity/WelcomeActivity.kt
+++ app/src/main/java/com/example/welcomeactivity/WelcomeActivity.kt
@@ -1,12 +1,19 @@
 package com.example.welcomeactivity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedButton.setOnClickListener {
+            val leagueIntent = Intent(this, leagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
